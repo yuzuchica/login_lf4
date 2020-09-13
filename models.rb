b7921865ee4b52336c3403013340900cc325bcd8
“{yuzuchica}” <{yuzuka0925@i.softbank.jp}>
2020-09-13 13:53:06 +0000

    first commit

---
+++ models.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :mail,
+    presence: true,
+    format: {with:/.+@.+/}
+  validates :password,
+    length: {in: 5..10}
+end
